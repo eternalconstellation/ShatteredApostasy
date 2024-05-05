3922d634f780a979483f777657ce142ae6b8ad8f
Lev <aaronpolon7@gmail.com>
2024-05-05 19:47:13 +0200

    Cleaned code, do movement, do dogde

---
+++ Systems/KeybindSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace ShatteredApostasy.Systems
+{
+    public class KeybindSystem : ModSystem
+    {
+        public ModKeybind DebugResetVelocity { get; private set; }
+        public ModKeybind DebugGoUP { get; private set; }
+        public ModKeybind DebugGoDOWN { get; private set; }
+        public ModKeybind DebugStayStill { get; private set; }
+        public override void Load()
+        {
+            DebugResetVelocity = KeybindLoader.RegisterKeybind(Mod, "DebugResetVelocity", "Q");
+            DebugGoUP = KeybindLoader.RegisterKeybind(Mod, "DebugGoUp", "E");
+            DebugGoDOWN = KeybindLoader.RegisterKeybind(Mod, "DebugGoDown", "Z");
+            DebugStayStill = KeybindLoader.RegisterKeybind(Mod, "DebugStayStill", "Y");
+        }
+
+        public override void Unload()
+        {
+            DebugGoUP = null;
+            DebugResetVelocity = null;
+            DebugGoDOWN = null;
+            DebugStayStill = null;
+        }
+    }
+}
